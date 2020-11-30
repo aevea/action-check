8be76ae427a6ac04fe699ec902b1772fd3c43af6
Simon Prochazka <simon@aevea.io>
2020-11-30 13:01:53 +0000

    chore: use struct instead of Value for Files

--- src/github/get_pr_files.rs
+++ src/github/get_pr_files.rs
@@ -2,7 +2,12 @@ use github_rs::{
     client::{Executor, Github},
     errors::Error,
 };
-use serde_json::Value;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct File {
+    pub filename: String,
+}
 
 /// get_pr_files gets the files from a given PR from a given organization repo
 pub fn get_pr_files(
@@ -10,7 +15,7 @@ pub fn get_pr_files(
     owner: &str,
     repo: &str,
     pr_id: &str,
-) -> Result<Option<Value>, Error> {
+) -> Result<Option<Vec<File>>, Error> {
     let pr_data = client
         .get()
         .repos()
@@ -19,7 +24,7 @@ pub fn get_pr_files(
         .pulls()
         .number(pr_id)
         .files()
-        .execute::<Value>();
+        .execute::<Vec<File>>();
 
     match pr_data {
         Ok((_headers, _status, json)) => Ok(json),
